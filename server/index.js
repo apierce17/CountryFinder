e5acada4471ddf74977cd5b7a363587b878f8518
Ashton Pierce <coalpierce@gmail.com>
2022-11-13 11:19:21 -0600

    [Server] Fetch countries from API
    /countries endpoint now returns all countries from API

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // import dependancies
 const express = require('express');
+const fetch = require('node-fetch');
 
 // create new express app
 const app = express();
@@ -7,9 +8,18 @@ const app = express();
 // server configuration
 const PORT = 3001;
 
+// Get list of countries
+const fetchCountries = async () => {
+    const response = await fetch(
+      `https://restcountries.com/v2/all`
+    );
+    const data = await response.json();
+    return data;
+};
+
 // create /countries endpoint
 app.get('/countries', async (req, res) => {
-    res.send('Countries endpoint')
+    res.status(200).json(await fetchCountries());
 });
 
 // make the server listen to requests
